ee04c27754abd8f0af23e98eb38f6a67c0211af3
chizz <cbrittingham@gmail.com>
2020-02-27 17:34:33 -0600

    added code to generate buttons dynamically based on an array of questions. each button has an index identifier which can be compared to the index of the correct answer.

--- script.js
+++ script.js
@@ -4,11 +4,36 @@ var wrapper = document.getElementsByClassName("wrapper");
 var cardEl = document.getElementById("card");
 var interval;
 var initalTime = 10;
+var questionArray = [
+    {question: "How much would can a wood chuck chuck?",
+    answers: ["none", "some", "a lot"],
+    correctIndex: 2,
+    }
+];
+
+console.log(questionArray[0]);
+console.log(questionArray[0].correctIndex)
+console.log(questionArray[0].answers[questionArray[0].correctIndex]);
 
 
-var btn = document.createElement("button");
-btn.textContent = "button";
-cardEl.append(btn);
+// var btn = document.createElement("button");
+// btn.textContent = "button";
+// cardEl.append(btn);
+
+for(i=0; i< questionArray[0].answers.length; i++){
+
+    var btn = document.createElement("button");
+    btn.className = "answer-button";
+    btn.textContent = questionArray[0].answers[i];
+    btn.id = i
+    cardEl.append(btn);
+    btn.addEventListener("click", function(){
+        console.log(this.id);
+        
+    })
+}
+
+
 
 interval = setInterval(function(){
     initalTime--
@@ -18,8 +43,6 @@ interval = setInterval(function(){
     timeEl.textContent = initalTime;
 }, 1000)
 
-btn.addEventListener("click", function(){
-    console.log("you clicked me");
-})
+
 
 
