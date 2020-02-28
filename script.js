d694868a7b7e4936b4a831129284e733dcd2b838
chizz <cbrittingham@gmail.com>
2020-02-27 22:08:34 -0600

    app is functional but score is not sorted and highscores link is not connected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 //Define variables
-var hsEl = document.getElementById("highscores");
+var h1El = document.getElementById("highscores-header");
 var timeEl = document.getElementById("time");
 var wrapper = document.getElementsByClassName("wrapper");
 var cardEl = document.getElementById("card");
@@ -8,7 +8,6 @@ var questionText = document.getElementById("question-text");
 var buttonHolder = document.getElementById("button-div");
 var interval;
 var score;
-var isOver = false;
 var initalTime = 100;
 var questionIndex = 0;
 var questionArray = [
@@ -35,36 +34,38 @@ var questionArray = [
 ];
 
 const init = function(){
+
     initalTime = 100;
     questionIndex = 0;
-    isOver = false;
     timeEl.textContent = "Time Remaining:  " + initalTime;
-    console.log("we initialized");
+
+    interval = setInterval(function(){
+        initalTime--
+        if (initalTime === 0){
+            clearInterval(interval);
+            alert("You lost! You suck!");
+        }
+        timeEl.textContent = "Time remaining:  " + initalTime;
+    }, 1000)
+   
 }
 const clickHandler = function(){
-
-    console.log("questionIndex.length ", questionArray.length);
-    console.log("questionIndex", questionIndex)
 
     if (parseInt(this.id) === questionArray[questionIndex].correctIndex){
         questionIndex++
     } else{
         initalTime-=7;
-        timeEl.textContent = initalTime;
+        timeEl.textContent = "Time remaining:  " + initalTime;
         questionIndex++
     }
     if(questionIndex < questionArray.length){
         setQuestion(questionIndex);
     } else {
-        console.log("thats it, question index, questionarray", questionIndex, "  ", questionArray);
+        endGame()
     }
 }
 
 const setQuestion = function(index){
-
-    // if(questionIndex === questionArray.length - 1){
-    //     isOver = true;
-    // }
 
     buttonHolder.innerHTML = "";
     questionText.textContent = questionArray[index].question;
@@ -81,41 +82,50 @@ const setQuestion = function(index){
     }
 }
 
+const endGame = function(){
+
+    clearInterval(interval);
+    h1El.innerHTML = "Highscores";
+    buttonHolder.innerHTML = ""
+    questionText.textContent = ""
+
+    score = initalTime;
+    var name = prompt("Please enter your initials");
+
+
+    if(!localStorage.getItem("scores")){
+
+        var scoreObject = [{"score": score, "initials": name}];
+
+        console.log("score object: ", scoreObject);
+
+        localStorage.setItem("scores", JSON.stringify(scoreObject));
+
+        var li = document.createElement("li");
+        li.textContent = name + "          " + score;
+        questionText.append(li);
+
+
+    } else{
+
+        var retrievedValues = JSON.parse(localStorage.getItem("scores"))
+        retrievedValues.push({"score": score, "initials": name});
+        console.log(retrievedValues);
+
+        for(i=0; i<retrievedValues.length; i++){
+            var li = document.createElement("li");
+            li.textContent = retrievedValues[i]["initials"] + "          " + retrievedValues[i]["score"];
+            questionText.append(li);
+        }
+
+        localStorage.setItem("scores", JSON.stringify(retrievedValues));
+    }
+}
 init();
 setQuestion(questionIndex);
 
-// var btn = document.createElement("button");
-// btn.textContent = "button";
-// cardEl.append(btn);
-
-// for(i=0; i< questionArray[0].answers.length; i++){
-
-//     var btn = document.createElement("button");
-//     btn.className = "answer-button";
-//     btn.textContent = questionArray[0].answers[i];
-//     btn.id = i
-//     cardEl.append(btn);
-//     btn.addEventListener("click", function(){
-//         console.log(this.id);
-//         if (parseInt(this.id) === 2||this.id === "2"){
-//             console.log("correct");
-//         } else{
-
-//             console.log("wrong");
-//         }
-        
-//     })
-// }
 
 
-
-interval = setInterval(function(){
-    initalTime--
-    if (initalTime === 0){
-        clearInterval(interval);
-    }
-    timeEl.textContent = initalTime;
-}, 1000)
 
 
 
