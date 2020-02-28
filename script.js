f1f59d40efe9c417ef365873bfece7cfe8dbbf51
chizz <cbrittingham@gmail.com>
2020-02-27 23:25:47 -0600

    patched up the highscore link.  added comments

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
 //Define variables
 var h1El = document.getElementById("highscores-header");
+var highScoreEl = document.getElementById("highscores");
 var timeEl = document.getElementById("time");
 var wrapper = document.getElementsByClassName("wrapper");
 var cardEl = document.getElementById("card");
 var questionText = document.getElementById("question-text");
 var buttonHolder = document.getElementById("button-div");
 var interval;
+var onHighScore = false;
+var skipPrompt = false;
 var score;
 var initalTime = 100;
 var questionIndex = 0;
+
+//set question array
 var questionArray = [
 
     {question: "Commonly used data types in Java script do not include: ",
@@ -32,32 +37,40 @@ var questionArray = [
     correctIndex: 2,
     }
 ];
-
+//reset values when page loads
 const init = function(){
 
+    clearInterval(interval);
+    highScoreEl.innerHTML = "Highscores";
     initalTime = 100;
     questionIndex = 0;
     timeEl.textContent = "Time Remaining:  " + initalTime;
+    onHighScore = false;
+    skipPrompt = false;
 
     interval = setInterval(function(){
         initalTime--
         if (initalTime === 0){
             clearInterval(interval);
-            alert("You lost! You suck!");
+            alert("You lost! You suck! No scores for you! But you're gonna try again");
+            init()
         }
         timeEl.textContent = "Time remaining:  " + initalTime;
     }, 1000)
+
+    setQuestion(questionIndex);
    
 }
+//
 const clickHandler = function(){
 
-    if (parseInt(this.id) === questionArray[questionIndex].correctIndex){
+    if (parseInt(this.id) === questionArray[questionIndex].correctIndex){//check if answer was correct
         questionIndex++
-    } else{
-        initalTime-=7;
+    } else{//penalize a wrong answer
+        initalTime-=12;
         timeEl.textContent = "Time remaining:  " + initalTime;
         questionIndex++
-    }
+    }//check if we have reached end of questionArray. end game if so
     if(questionIndex < questionArray.length){
         setQuestion(questionIndex);
     } else {
@@ -67,9 +80,11 @@ const clickHandler = function(){
 
 const setQuestion = function(index){
 
+    //reset markup for new question
     buttonHolder.innerHTML = "";
     questionText.textContent = questionArray[index].question;
 
+    //make a button for each answer in the question array object
     for(i=0; i< questionArray[index].answers.length; i++){
 
         var btn = document.createElement("button");
@@ -84,45 +99,75 @@ const setQuestion = function(index){
 
 const endGame = function(){
 
+    onHighScore = true;
+    highScoreEl.innerHTML = "Restart Quiz";
+
+    //reset markup and timer
     clearInterval(interval);
     h1El.innerHTML = "Highscores";
     buttonHolder.innerHTML = ""
     questionText.textContent = ""
 
+    //get score and initials
     score = initalTime;
-    var name = prompt("Please enter your initials");
+    if(!skipPrompt){
+        var name = prompt("Please enter your initials");
+    } 
+    
 
-
+    //check if scores object exists already
     if(!localStorage.getItem("scores")){
 
+        //create score object (as array) if it does not exist
         var scoreObject = [{"score": score, "initials": name}];
-
-        console.log("score object: ", scoreObject);
-
         localStorage.setItem("scores", JSON.stringify(scoreObject));
 
+        //add score to the markup dynamically
         var li = document.createElement("li");
         li.textContent = name + "          " + score;
         questionText.append(li);
 
 
-    } else{
+    } else {
 
+        //get the score values that are saved in local storage
         var retrievedValues = JSON.parse(localStorage.getItem("scores"))
-        retrievedValues.push({"score": score, "initials": name});
-        console.log(retrievedValues);
 
+        if(!skipPrompt){
+            retrievedValues.push({"score": score, "initials": name}); // add the current score to the array
+        }
+        
+
+        //write each element in the array to the markup
         for(i=0; i<retrievedValues.length; i++){
             var li = document.createElement("li");
             li.textContent = retrievedValues[i]["initials"] + "          " + retrievedValues[i]["score"];
             questionText.append(li);
         }
 
-        localStorage.setItem("scores", JSON.stringify(retrievedValues));
+        localStorage.setItem("scores", JSON.stringify(retrievedValues)); //save the new score array
     }
 }
 init();
 setQuestion(questionIndex);
+
+//handel highscore link without new html page
+highScoreEl.addEventListener("click", function(){
+
+    event.preventDefault(); //do not refresh page on click
+
+    if(!onHighScore && !localStorage.getItem("scores")){ // make sure there are highscores and you are not already on the page
+        alert("No highscores to view! Take the quiz first");
+        init();
+    } else if(!onHighScore){
+
+        skipPrompt = true; //boolean to ensure invalid scores are not saved
+        endGame();
+
+    } else {
+        init();
+    }
+})
 
 
 
