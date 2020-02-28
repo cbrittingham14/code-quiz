49283750e0a8f40ddf61b501ce329936490beebe
chizz <cbrittingham@gmail.com>
2020-02-27 20:37:43 -0600

    made logic track questions and stop when end of question array is reached

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ var timeEl = document.getElementById("time");
 var wrapper = document.getElementsByClassName("wrapper");
 var cardEl = document.getElementById("card");
 var questionText = document.getElementById("question-text");
+var buttonHolder = document.getElementById("button-div");
 var interval;
+var score;
+var isOver = false;
 var initalTime = 100;
 var questionIndex = 0;
 var questionArray = [
@@ -34,36 +37,75 @@ var questionArray = [
 const init = function(){
     initalTime = 100;
     questionIndex = 0;
+    isOver = false;
     timeEl.textContent = "Time Remaining:  " + initalTime;
     console.log("we initialized");
 }
+const clickHandler = function(){
+
+    console.log("questionIndex.length ", questionArray.length);
+    console.log("questionIndex", questionIndex)
+
+    if (parseInt(this.id) === questionArray[questionIndex].correctIndex){
+        questionIndex++
+    } else{
+        initalTime-=7;
+        timeEl.textContent = initalTime;
+        questionIndex++
+    }
+    if(questionIndex < questionArray.length){
+        setQuestion(questionIndex);
+    } else {
+        console.log("thats it, question index, questionarray", questionIndex, "  ", questionArray);
+    }
+}
+
+const setQuestion = function(index){
+
+    // if(questionIndex === questionArray.length - 1){
+    //     isOver = true;
+    // }
+
+    buttonHolder.innerHTML = "";
+    questionText.textContent = questionArray[index].question;
+
+    for(i=0; i< questionArray[index].answers.length; i++){
+
+        var btn = document.createElement("button");
+        btn.className = "answer-button";
+        btn.textContent = questionArray[index].answers[i];
+        btn.id = i
+        buttonHolder.append(btn);
+        btn.addEventListener("click", clickHandler)
+        
+    }
+}
 
 init();
+setQuestion(questionIndex);
 
-
-questionText.textContent = questionArray[0].question
 // var btn = document.createElement("button");
 // btn.textContent = "button";
 // cardEl.append(btn);
 
-for(i=0; i< questionArray[0].answers.length; i++){
+// for(i=0; i< questionArray[0].answers.length; i++){
 
-    var btn = document.createElement("button");
-    btn.className = "answer-button";
-    btn.textContent = questionArray[0].answers[i];
-    btn.id = i
-    cardEl.append(btn);
-    btn.addEventListener("click", function(){
-        console.log(this.id);
-        if (parseInt(this.id) === 2||this.id === "2"){
-            console.log("correct");
-        } else{
+//     var btn = document.createElement("button");
+//     btn.className = "answer-button";
+//     btn.textContent = questionArray[0].answers[i];
+//     btn.id = i
+//     cardEl.append(btn);
+//     btn.addEventListener("click", function(){
+//         console.log(this.id);
+//         if (parseInt(this.id) === 2||this.id === "2"){
+//             console.log("correct");
+//         } else{
 
-            console.log("wrong");
-        }
+//             console.log("wrong");
+//         }
         
-    })
-}
+//     })
+// }
 
 
 
@@ -74,7 +116,6 @@ interval = setInterval(function(){
     }
     timeEl.textContent = initalTime;
 }, 1000)
-
 
 
 
